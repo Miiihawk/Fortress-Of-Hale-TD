a685096c2ba67d962628ee1544e881a56264b7e9
Miiihawk <131926009+Miiihawk@users.noreply.github.com>
2025-04-01 20:10:28 +0800

    new scene
    new scene

--- Assets/Turret.cs
+++ Assets/Turret.cs
@@ -46,14 +46,6 @@ public class Turret : MonoBehaviour
         }
     }
 
-    //private void Shoot()
-    //{
-    //    GameObject bulletObj = Instantiate(bulletPrefab, firingPoint.position, Quaternion.identity);
-    //    Bullet bulletScript = bulletObj.GetComponent<Bullet>();
-    //    bulletScript.SetTarget(target);
-    //}
-
-
     private void Shoot()
     {
         // Randomly select a bullet prefab from the array
